a6783cf81ca49816e3ecc8a4c4e0461c5cdae97c
Nikolay Toporkov <toporkov.kolya96@gmail.com>
2019-08-12 17:44:10 +0300

    deploy token in migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,8 @@
 const DAPSSwap = artifacts.require("DAPSSwap");
+// const DAPSTOKEN = artifacts.require("DAPSTOKEN");
 
-module.exports = function(deployer) {
+module.exports = function(deployer, network, [owner]) {
+  // deployer.deploy(DAPSTOKEN, owner, 1000).then(dapsToken => deployer.deploy(DAPSSwap, dapsToken.address));
+
   deployer.deploy(DAPSSwap, process.env.DAPS_ERC_ADDRESS);
 };
