b0a3e2919623b63e6cbd55b728e6221d427a8a21
Nikolay Toporkov <toporkov.kolya96@gmail.com>
2019-08-13 13:10:08 +0300

    add ability to reconnect address

--- contracts/DAPSSwap.sol
+++ contracts/DAPSSwap.sol
@@ -4,11 +4,17 @@ import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 import "./Transferable.sol";
 
 contract DAPSSwap is Ownable {
+    uint256 internal constant MAX_CHANGE_TIMES = 5;
+
     Transferable public transToken;
+    mapping(address => string[]) internal _registers;
 
-    mapping(address => string) public registers;
-
-    event Put(address indexed eth, string indexed daps);
+    event Connect(address indexed eth, string indexed daps);
+    event Reconnect(
+        address indexed eth,
+        string indexed oldDaps,
+        string indexed newDaps
+    );
 
     constructor(Transferable _transToken) public {
         transToken = _transToken;
@@ -19,17 +25,55 @@ contract DAPSSwap is Ownable {
         transToken.transfer(address(0), balance);
     }
 
-    function put(string calldata _daps) external {
-        require(bytes(registers[msg.sender]).length == 0, "Already registered");
-        registers[msg.sender] = _daps;
-        emit Put(msg.sender, _daps);
+    function connect(string calldata _daps) external {
+        address eth = msg.sender;
+
+        if (_registers[eth].length == 0) {
+            emit Connect(eth, _daps);
+            _registers[eth].push(_daps);
+        } else {
+            require(
+                _registers[eth].length < MAX_CHANGE_TIMES,
+                "Address change limit exceeded"
+            );
+            emit Reconnect(eth, getCurrentDAPSAddress(eth), _daps);
+            _registers[eth].push(_daps);
+        }
     }
 
-    function burn(Transferable token, uint256 value) external {
-        token.transfer(address(0), value);
+    function burn(Transferable _token, uint256 _value) external {
+        _token.transfer(address(0), _value);
     }
 
-    function burnFrom(Transferable token, address from, uint256 value) external {
-        token.transferFrom(from, address(0), value);
+    function burnFrom(Transferable _token, address _from, uint256 _value)
+        external
+    {
+        _token.transferFrom(_from, address(0), _value);
+    }
+
+    function isConnected(address _eth) public view returns (bool) {
+        return _registers[_eth].length != 0;
+    }
+
+    function getCurrentDAPSAddress(address _eth)
+        public
+        view
+        returns (string memory)
+    {
+        if (!isConnected(_eth)) {
+            string memory empty;
+            return empty;
+        }
+
+        uint256 count = _registers[_eth].length;
+        return _registers[_eth][count - 1];
+    }
+
+    function getDAPSAddressByIndex(address _eth, uint256 _index)
+        public
+        view
+        returns (string memory)
+    {
+        return _registers[_eth][_index];
     }
 }
--- test/DAPSSwap.test.js
+++ test/DAPSSwap.test.js
@@ -28,45 +28,71 @@ contract("DAPS swap", ([owner, ...accounts]) => {
     expect(await token.totalSupply()).to.eq.BN(0);
   });
 
-  it("put long address", async () => {
-    let addressDapsLong =
+  it("connect long address", async () => {
+    const addressDapsLong =
       "41kYDmcd27f2ULWE6tfC19UnEHYpEhMBtfiYwVFUYbZhXrjLomZXSovQPGzwTCAgwQLpWiEQPA5uyNjmEVLPr4g71AUMNjaVD3n";
-    await dapsSwap.put(addressDapsLong);
-    expect(await dapsSwap.registers(owner)).to.equal(addressDapsLong);
+    expect(await dapsSwap.isConnected(owner)).to.be.equal(false);
+    await dapsSwap.connect(addressDapsLong);
+    expect(await dapsSwap.isConnected(owner)).to.be.equal(true);
+    expect(await dapsSwap.getCurrentDAPSAddress(owner)).to.equal(addressDapsLong);
   });
 
-  it("put short address", async () => {
-    let addressDapsShort = "DEQsu2RRB5iphm9tKXiP4iWSRMC17gseW5";
-    await dapsSwap.put(addressDapsShort);
-    expect(await dapsSwap.registers(owner)).to.equal(addressDapsShort);
+  it("connect short address", async () => {
+    const addressDapsShort = "DEQsu2RRB5iphm9tKXiP4iWSRMC17gseW5";
+    await dapsSwap.connect(addressDapsShort);
+    expect(await dapsSwap.getCurrentDAPSAddress(owner)).to.equal(addressDapsShort);
   });
 
-  it("put some another addresses", async () => {
-    let addressDapsShort = "DEQsu2RRB5iphm9tKXiP4iWSRMC17gseW5";
-    let addressDapsLong =
+  it("connect some another addresses", async () => {
+    const addressDapsShort = "DEQsu2RRB5iphm9tKXiP4iWSRMC17gseW5";
+    const addressDapsLong =
       "41kYDmcd27f2ULWE6tfC19UnEHYpEhMBtfiYwVFUYbZhXrjLomZXSovQPGzwTCAgwQLpWiEQPA5uyNjmEVLPr4g71AUMNjaVD3n";
 
-    await dapsSwap.put(addressDapsShort, {from: accounts[0]});
-    await dapsSwap.put(addressDapsShort, {from: accounts[1]});
-    await dapsSwap.put(addressDapsLong, {from: accounts[2]});
-    await dapsSwap.put(addressDapsLong, {from: accounts[3]});
+    await dapsSwap.connect(addressDapsShort, {from: accounts[0]});
+    await dapsSwap.connect(addressDapsShort, {from: accounts[1]});
+    await dapsSwap.connect(addressDapsLong, {from: accounts[2]});
+    await dapsSwap.connect(addressDapsLong, {from: accounts[3]});
 
-    expect(await dapsSwap.registers(accounts[0])).to.equal(addressDapsShort);
-    expect(await dapsSwap.registers(accounts[1])).to.equal(addressDapsShort);
-    expect(await dapsSwap.registers(accounts[2])).to.equal(addressDapsLong);
-    expect(await dapsSwap.registers(accounts[3])).to.equal(addressDapsLong);
+    expect(await dapsSwap.isConnected(accounts[0])).to.be.equal(true);
+
+    expect(await dapsSwap.getCurrentDAPSAddress(accounts[0])).to.equal(addressDapsShort);
+    expect(await dapsSwap.getCurrentDAPSAddress(accounts[1])).to.equal(addressDapsShort);
+    expect(await dapsSwap.getCurrentDAPSAddress(accounts[2])).to.equal(addressDapsLong);
+    expect(await dapsSwap.getCurrentDAPSAddress(accounts[3])).to.equal(addressDapsLong);
   });
 
-  it("can not put more then 1 address per contract", async () => {
-    let addressDapsLong =
+  it("can reconnect", async () => {
+    const addressDapsLong =
       "41kYDmcd27f2ULWE6tfC19UnEHYpEhMBtfiYwVFUYbZhXrjLomZXSovQPGzwTCAgwQLpWiEQPA5uyNjmEVLPr4g71AUMNjaVD3n";
-    await dapsSwap.put(addressDapsLong);
-    await expectRevert(dapsSwap.put(addressDapsLong), "Already registered");
+    await dapsSwap.connect(addressDapsLong);
+    const anotherDapsAddress = "abcdabcd";
+    await dapsSwap.connect(anotherDapsAddress);
+    expect(await dapsSwap.getCurrentDAPSAddress(owner)).to.equal(anotherDapsAddress);
+  });
+
+  it("stores all connected addresses", async () => {
+    const addresses = ["addr1", "addr2", "addr3"];
+    for (let addr of addresses) {
+      await dapsSwap.connect(addr);
+    }
+    expect(await dapsSwap.isConnected(owner)).to.be.equal(true);
+    for (let index in addresses) {
+      const address = await dapsSwap.getDAPSAddressByIndex(owner, index);
+      expect(address).to.be.equal(addresses[index]);
+    }
+  });
+
+  it("can't reconnect more than 5 times", async () => {
+    const addresses = ["a1", "a2", "a3", "a4", "a5"];
+    for (let addr of addresses) {
+      await dapsSwap.connect(addr);
+    }
+    await expectRevert(dapsSwap.connect("a6"), "Address change limit exceeded");
   });
 
   it("send, burn, send, burn...", async () => {
-    let count = 5;
-    let sum = (await token.balanceOf(accounts[0])) / count;
+    const count = 5;
+    const sum = (await token.balanceOf(accounts[0])) / count;
     for (let i = 0; i < count; i++) {
       await token.transfer(dapsSwap.address, sum, {from: accounts[0]});
       await dapsSwap.burn();
@@ -78,8 +104,8 @@ contract("DAPS swap", ([owner, ...accounts]) => {
   });
 
   it("send, send, burn", async () => {
-    let count = 5;
-    let sum = (await token.balanceOf(accounts[0])) / count;
+    const count = 5;
+    const sum = (await token.balanceOf(accounts[0])) / count;
 
     for (let i = 0; i < count; i++) {
       await token.transfer(dapsSwap.address, sum, {from: accounts[0]});
